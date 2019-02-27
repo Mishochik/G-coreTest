cff9fd9ae220e990fdf3139eb41742cecf922037
Mishochik <mixailoparin@gmail.com>
2019-02-27 21:37:22 +0500

    header, routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FavoriteBooksComponent } from './favorite-books/favorite-books.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+const appRoutes = [
+    { path: '', component: HomeComponent },
+    { path: 'FavoriteBooks', component: FavoriteBooksComponent },
+    { path: '**', component: NotFoundComponent }
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        FavoriteBooksComponent,
+        NotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
